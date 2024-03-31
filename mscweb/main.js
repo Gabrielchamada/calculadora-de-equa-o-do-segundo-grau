37a8258e09453cd95199943f8194f7f907a805c3
Gabrielchamada <80106399+Gabrielchamada@users.noreply.github.com>
2024-03-31 00:42:42 -0300

    Add files via upload

--- mscweb/main.js
+++ mscweb/main.js
@@ -13,9 +13,34 @@ btn_calculate_click.addEventListener("click",
         var raiz = Math.sqrt(delta);
         const x_one = (-value_B + raiz / 2 * value_A);
         const x_two = (-value_B - raiz / 2 * value_A);
-        //innerhtml of delta
+        //innerhtml of values and examples
         document.getElementById("delta_value").innerHTML="Delta: " + delta;
         document.getElementById("x_one_value").innerHTML="valor do primeiro x: x+ = " + x_one;
         document.getElementById("x_two_value").innerHTML="valor do segundo x: x- = " + x_two;
-    }   
+        document.getElementById("delta_formula_exampleone").innerHTML="a fórmula irá ficar: Δ = " + value_B + "² - 4" + "." + value_A + "." + value_C;
+        document.getElementById("delta_formula_exampletwo").innerHTML="resolvendo teremos: Δ = " + delta;
+        document.getElementById("baskhara_formula_exampleone").innerHTML="a fórmula irá ficar: x = -" + value_B + " ± √" + delta + " /2." + value_A;
+        document.getElementById("baskhara_formula_exampletwo").innerHTML="resolvendo teremos: x+ = " + x_one + "<br>" + "x- = " + x_two;
+    }
+);
+
+//shadows on button
+btn_calculate_click.addEventListener("mouseenter", 
+    function animation_btn_enter(){
+        btn_calculate_click.style.boxShadow = "5px 5px 5px black";
+    }
+);
+
+btn_calculate_click.addEventListener("mouseleave", 
+    function animation_btn_leave(){
+        btn_calculate_click.style.boxShadow = "none";
+    }
+);
+//examples visible or invisible
+const example_div = document.getElementById("how");
+
+btn_calculate_click.addEventListener("click", 
+    function visible(){
+            example_div.classList.toggle("hidden");
+    }
 );
